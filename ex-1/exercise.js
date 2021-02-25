ac96e6e212b48f0951a7ad68c724f58d06bc00c0
Joshua Udeh <udehasilo@gmail.com>
2021-02-25 12:03:29 +0100

    exercise 1

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,2 +1,9 @@
 // Example
-window.alert("Hello World!");
+console.log("Hello World!");
+var Name = "Joshua";
+var NumberOfChildren = 3;
+var PartnersName = "sophia";
+var GeographicalLocation = "Italy";
+var JobTile = "software developer";
+console.log("I am " + Name + " a " + JobTile +  " in " + GeographicalLocation + " and married to " + PartnersName + " with " + NumberOfChildren + " kids");
+window.alert("I am " + Name + " a " + JobTile +  " in " + GeographicalLocation + " and married to " + PartnersName + " with " + NumberOfChildren + " kids");
